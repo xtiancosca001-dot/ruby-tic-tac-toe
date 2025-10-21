e191169216cb6f5d6ecc764684530bd3b3ea4e99
xtiancosca001-dot <christiancosca@gmail.com>
2025-10-21 16:55:45 +0800

    Add winning flag for player

--- lib/tic_tac_toe/player.rb
+++ lib/tic_tac_toe/player.rb
@@ -1,9 +1,10 @@
 # Player Object Class File
 class Player
-  attr_accessor :name, :moves, :move_profile
+  attr_accessor :name, :moves, :move_profile, :is_winner
 
   def initialize(name, move_profile)
     @name = name
+    @is_winner = false
     raise "move_profile is not accepted (Entered #{move_profile}), acceptable: x / o" unless (move_profile == 'o' || move_profile == 'x')
     @move_profile = move_profile
     @moves = []
