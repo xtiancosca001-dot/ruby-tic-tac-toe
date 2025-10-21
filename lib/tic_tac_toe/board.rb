f882faaafd69022623418095a70f48755bc77e81
xtiancosca001-dot <christiancosca@gmail.com>
2025-10-21 16:57:15 +0800

    Make board as per instance

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,30 +1,32 @@
 class Board
   ROWS = COLUMNS = 3
 
-  @@moves = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9']
-  ]
-
-  def self.clear_board
-    @@moves = [
+  def initialize
+    @moves = [
       ['1','2','3'],
       ['4','5','6'],
       ['7','8','9']
     ]
   end
 
-  def self.update_board(x,y,value)
+  def clear_board
+    @moves = [
+      ['1','2','3'],
+      ['4','5','6'],
+      ['7','8','9']
+    ]
+  end
+
+  def update_board(x,y,value)
     raise "x position is out of range (entered #{x})" unless x < ROWS
     raise "y position is out of range (entered #{y})" unless y < COLUMNS
     raise "Value is not acceptable (entered #{value}) (only: x / o)" unless (value == 'o' || value == 'x')
-    @@moves[x][y] = value
+    @moves[x][y] = value
   end
 
-  def self.to_s
+  def to_s
     grid = "+---+---+---+\n"
-    @@moves.each do |line|
+    @moves.each do |line|
       grid += "| #{line.join(" | ")} |\n"
       grid += "+---+---+---+\n"
     end
