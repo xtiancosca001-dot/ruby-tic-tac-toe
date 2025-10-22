80934c84f08b6441bd8a896e36e9de1efbf6aa32
xtiancosca001-dot <christiancosca@gmail.com>
2025-10-22 11:19:41 +0800

    Revise game sequence logic based on turns

--- lib/tic_tac_toe_game.rb
+++ lib/tic_tac_toe_game.rb
@@ -4,17 +4,20 @@ require_relative 'tic_tac_toe/player'
 class TicTacToeGame
   attr_accessor :player_1, :player_2
 
+  @@players = 0
+
   def initialize
     @board = Board.new
     @moves = (1..9).to_a
     setup_game
+    begin_game
   end
 
   def set_player_info(player)
     print "> Please enter player's name: "
-    player = Player.new
     player_name = gets.chomp
-    player.name = player_name.empty? ? "PLAYER 1" : player_name
+    @@players += 1
+    player.name = player_name.empty? ? "PLAYER #{@@players}" : player_name
   end
 
   def set_player_profiles
@@ -55,30 +58,60 @@ class TicTacToeGame
     "Player 1 (x): #{@player_1.name} | Player 2 (o): #{@player_2.name}"
   end
 
-  def get_player_move(player)
-    puts "#{player.name}'s TURN (#{player.move_profile})"
-    input_move = gets.chomp.to_i
+  def get_player_move(player, input_move)
     move = player.move(input_move)
     @board.update_board move[0], move[1], player.move_profile
+    @moves.delete input_move
     player.is_winner = true if validate_moves(player)
   end
 
   def begin_game
     puts "Select a number indicating the position of your move in the board below:"
     puts @board
+    puts
 
-    1.upto(9) do |turn|
+    has_one_winner = @player_1.is_winner || @player_2.is_winner
+    @player_1.has_moved, @player_2.has_moved = false, true
+    while @moves.length > 0 && !has_one_winner
+      player_1_turn = @player_2.has_moved
+      player_turn = player_1_turn ? @player_1 : @player_2
+      puts "#{player_turn.name}'s TURN (#{player_turn.move_profile})"
+
       puts "MOVES LEFT: #{@moves}"
-      unless turn % 2 == 0
-        get_player_move(@player_1)
-      else  
-        get_player_move(@player_2)
+      input_move = gets.chomp
+      break if input_move == 'quit'
+        
+      if input_move.to_i.between?(1,9) && @moves.include?(input_move.to_i)
+        if player_1_turn
+          get_player_move(@player_1, input_move.to_i)
+          @player_1.has_moved, @player_2.has_moved = true, false
+        else
+          get_player_move(@player_2, input_move.to_i)
+          @player_1.has_moved, @player_2.has_moved = false, true
+        end
+      elsif input_move.to_i == 0 || input_move.to_i > 9
+        puts "INVALID MOVE! Please try again. (accepted: 1-9)"
+      elsif !@moves.include?(input_move)
+        puts "Move already done. Try another move."
       end
-      break if @player_1.is_winner || @player_2.is_winner
-      @moves.delete input_move
+
       puts @board
       puts
+      break if (@player_1.is_winner || @player_2.is_winner)
+      
     end
+    # 1.upto(9) do |turn|
+    #   puts "MOVES LEFT: #{@moves}"
+    #   input_move = gets.chomp.to_i 
+    #   unless turn % 2 == 0
+    #     get_player_move(@player_1, input_move)
+    #   else  
+    #     get_player_move(@player_2, input_move)
+    #   end
+    #   puts @board
+    #   puts
+    #   break if @player_1.is_winner || @player_2.is_winner
+    # end
 
     evaluate_game
   end
