fbbadc2cf8ac5627a335152c34c7743d6d722e88
Christian Eric Cosca <ccosca@iripple.com>
2025-10-23 08:19:12 +0800

    Revise position variable names and add documentation

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,3 +1,4 @@
+# This is a 3x3 game board for tic tac toe
 class Board
   ROWS = COLUMNS = 3
 
@@ -17,12 +18,12 @@ class Board
     ]
   end
 
-  def update_board(x, y, value)
-    raise "x position is out of range (entered #{x})" unless x < ROWS
-    raise "y position is out of range (entered #{y})" unless y < COLUMNS
+  def update_board(x_position, y_position, value)
+    raise "x position is out of range (entered #{x_position})" unless x_position < ROWS
+    raise "y position is out of range (entered #{y_position})" unless y_position < COLUMNS
     raise "Value is not acceptable (entered #{value}) (only: x / o)" unless %w[o x].include?(value)
 
-    @moves[x][y] = value
+    @moves[x_position][y_position] = value
   end
 
   def to_s
